e72fdc66b6b1cb5eeec595e3d50a8c38997b8764
Guillermo <2820945@alu.murciaeduca.es>
2024-11-13 20:54:59 +0100

    Operadores

---
+++ Operadores.py
@@ -0,0 +1,43 @@
+### operadores Aritméticos ###
+
+## Operadores Enteros
+#Suma de dos números
+numero1=2
+numero2=4
+numero1+numero2
+print("Suma de dos números")
+print(numero1+numero2)
+#Resta
+print (numero1-numero2)
+#Multiplicar
+print (numero1*numero2)
+#División
+print (numero1/numero2)
+#Modulo (el resto)
+print (numero1%numero2)
+#División Entera sin decimales
+print (numero1//numero2)
+#Exponente
+print (numero1**numero2)
+#COMBINAR OPERACIONES
+print (2**3 - 7 / 1 //4)
+#Operaciones con texto
+print("Hola"+"Jose")
+#No se puede sumar letras con números porque es numero y hola es strem
+print("Hola "+ str(5))
+print("hola " *5)
+#No se puede multiplicar por decimaales
+my_float=2.5 * 2
+
+print(type(my_float))
+print("Hola" * int(my_float))
+
+#OPeraciones comparativos
+print(2<3)
+print(3>4)
+print(3>=4)
+print(3<4)
+print(3==4)
+print(3 != 4)
+
+print(numero1 != numero2) #Compara variables  si no son iguales

af0fc826e86404a1f9b5c9952a00dc4ea2d9261f
Guillermo <2820945@alu.murciaeduca.es>
2024-11-28 19:55:17 +0100

    ejercicios cadenas

--- 8_consola.py
+++ 8_consola.py
@@ -6,14 +6,36 @@
 # print (num1)
 
 # Hacer la suma de dos numeros dados
-print("---------------------------------------------------------")
-print("Dame numero")
-num1 =int(input())
-print("Dame primer un número no letras")
-print (num1)
-print("Dame numero")
-num2 =int(input())
-print("Dame segundo un número no letras")
-print (num1+num2)
-suma =num1+num2
-print("la suma es: " , suma)
+# print("---------------------------------------------------------")
+# print("Dame numero")
+# num1 =int(input())
+# print("Dame primer un número no letras")
+# print (num1)
+# print("Dame numero")
+# num2 =int(input())
+# print("Dame segundo un número no letras")
+# print (num1+num2)
+# suma =num1+num2
+# print("la suma es: " , suma)
+#Quiero que pidais por pantalla dos numeros y muestre el resultado
+#de la suma, resta, mutiplicaciones y división
+# print("Escribe un numero")
+# num3 =int(input())
+# print("Escribe un segundo número")
+# num4 =int(input())
+# print("----------------------------------------------------------------------------")
+# print ("La suma es:", num3+num4)
+# print ("La resta es:", num3-num4)
+# print ("La multiplciación es:", num3*num4)
+# print ("La división:", num3/num4)
+#Escribir un programa que pregunte el nombre completo del usuario en la consola y 
+# después muestre por pantalla el nombre completo del usuario tres veces, una con 
+# todas las letras minúsculas, otra con todas las letras mayúsculas y otra solo 
+# con la primera letra del nombre y de los apellidos en mayúscula. El usuario puede 
+# introducir su nombre combinando mayúsculas y minúsculas como quiera.
+#print("hola" \n * 5
+print ("Dime tu nombre completo:")
+nom =str(input())
+print ("Cuantas veces quieres repetir el nombre:")
+num_name =int(input())
+print((nom + "\n") * (num_name))
